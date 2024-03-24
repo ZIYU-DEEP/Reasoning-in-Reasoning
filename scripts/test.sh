c72110b554321ccdd47a4fa24b8ace1d93a71a03
Ziyu <ziyuye@uchicago.edu>
2024-03-24 16:25:40 -0400

    scripts

---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test.yaml --search_method bfs_low
+
+CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test.yaml --search_method bfs_low_with_raw_high
+
+CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test_mew_openai.yaml --search_method bfs_low
