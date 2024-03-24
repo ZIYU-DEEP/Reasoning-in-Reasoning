b49ce624545b1cc978741f422aebbbea1b591e71
Ziyu <ziyuye@uchicago.edu>
2024-03-24 16:30:26 -0400

    slice

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,6 @@ CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test.yaml --search_metho
 
 CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test.yaml --search_method bfs_low_with_raw_high
 
-CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test_mew_openai.yaml --search_method bfs_low
+CUDA_VISIBLE_DEVICES=1 python run.py --config_name dojo_test_mew_openai.yaml --search_method bfs_low 
+
+python run.py --config_name dojo_test_mew_openai.yaml --search_method bfs_low --slice_size 1
